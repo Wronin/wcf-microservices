a42a2f647d93f3129f57f4202250320890d8097d
Alexandr <31178187+calcq@users.noreply.github.com>
2021-05-12 20:32:15 +0300

    service contract updated

--- micro-notes/INoteService.cs
+++ micro-notes/INoteService.cs
@@ -1,10 +1,16 @@
 using System.Collections.Generic;
+using System.ServiceModel;
 
 namespace micro_notes {
+    [ServiceContract]
     public interface INoteService {
+        [OperationContract]
         void AddNote(Note note);
+        [OperationContract]
         ICollection<Note> GetNotes();
+        [OperationContract]
         void UpdateNote(int noteNumber);
+        [OperationContract]
         void RemoveNote(int noteNumber);
     }
 }
